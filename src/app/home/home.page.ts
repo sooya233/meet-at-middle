35b2f377a788d581142b1d897554c2680b630a5e
Im Jun Yeop <dunebi1030123@gmail.com>
2021-07-23 02:38:56 +0900

    click event

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +15,19 @@ export class HomePage {
     }
   }
 
-  constructor(public router:Router) {}
+  constructor(public router:Router, public alertController: AlertController) {}
   gotoFindPage() {
     this.router.navigateByUrl('/find-page');
+  }
+
+  async Alert(station:string) {
+    const alert = await this.alertController.create({
+      cssClass: 'basic-alert',
+      header: station,
+      message: "선택한 역이 맞습니까?",
+      buttons: ['Yes', 'No'],
+    });
+
+    await alert.present();
   }
 }
